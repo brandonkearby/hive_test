67e8f2ba73e84c101b99d9050a41b8ef57600864
Brandon Kearby <brandon.kearby@dachisgroup.com>
2013-04-30 08:45:10 -0500

    Added repo deployment stuff

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 mvn -e deploy:deploy-file \
 -DgroupId=com.jointhegrid \
 -DpomFile=pom.xml \
--DartifactId=hive_test \
--Dversion=0.10.0-SNAPSHOT \
--Dpackaging=jar \
--Dfile=target/hive_test-0.10.0-SNAPSHOT.jar \
 -DrepositoryId=dachis.repository.releases \
 -Durl=http://artifactory.dachisdev.com/libs-releases-local
